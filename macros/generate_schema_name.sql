1c11e3ab83b49673be2c3dc59931efeac1a1ad4c
danieljohnson726 <danieljohnson726@gmail.com>
2026-03-10 05:31:59 -0700

    customer schema changes

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,8 +1,8 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
     {# If a custom schema is set at model/folder, use it; otherwise use target.schema #}
     {% if custom_schema_name is not none %}
-        {{ custom_schema_name | upper }}
+        {{ custom_schema_name | lower }}
     {% else %}
-        {{ target.schema | upper }}
+        {{ target.schema | lower }}
     {% endif %}
 {%- endmacro %}
