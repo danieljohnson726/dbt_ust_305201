435fe555dab8f73a3ebd28ddf7b1cc898e9653c2
danieljohnson726 <danieljohnson726@gmail.com>
2026-03-11 03:14:16 -0700

    20260311_v1

--- models/intermediate/int_customers.sql
+++ models/intermediate/int_customers.sql
@@ -1,4 +1,8 @@
 select
-    *
+    c .* ,n.name as nation, r.name as region
 from
-    {{ ref('stg_customers') }}
+    {{ ref('stg_customers') }} c
+join {{ ref('stg_nations') }} n
+    on n.nationid = c.nation_id
+join {{ ref('stg_regions') }} r
+    on r.regionid = n.regionid
